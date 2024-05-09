c43bfc068d67d31abefa33cc2647b56412ab97a5
OmarAhmetovic21 <92754632+OmarAhmetovic21@users.noreply.github.com>
2024-05-09 13:41:13 +0200

    About us page, sidebar fixes, and home page image changed

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -12,6 +12,7 @@ import { ProfileComponent } from './examples/profile/profile.component';
 const routes: Routes =[
     { path: '', redirectTo: 'index', pathMatch: 'full' },
     { path: 'index',                component: ComponentsComponent },
+    { path: 'about-us',             component: AboutUsComponent },
     { path: 'examples/landing',     component: LandingComponent },
     { path: 'examples/login',       component: LoginComponent },
     { path: 'examples/profile',     component: ProfileComponent }
