1ec1a3876c346cf86ba26fd2fa72b21725acb5ab
OmarAhmetovic21 <92754632+OmarAhmetovic21@users.noreply.github.com>
2024-05-10 11:38:43 +0200

    Added getFixtures to fixture service

--- src/app/services/fixtures-service/fixtures.service.ts
+++ src/app/services/fixtures-service/fixtures.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse,HttpParams, HttpHeaders, HttpEvent, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FixturesService {
 
-  constructor() { }
+  private API_SERVER = environment.apiUrl;
+  constructor(private httpClient: HttpClient) { 
+
+  }
+
+  public getFixtures(): Observable<any>{
+    const headers = new HttpHeaders();
+    headers.set('Content-Type', 'application/json; charset=utf-8');  
+    return this.httpClient.get(this.API_SERVER + '/getFixtures.php',{headers: headers});
+  }
 }
